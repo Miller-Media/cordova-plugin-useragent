b81e7781f3dd985badb574480038b300eb29b551
Kevin Carwile <kevin@miller-media.com>
2020-05-19 16:41:40 -0700

    revert to working version

--- src/ios/UserAgent.m
+++ src/ios/UserAgent.m
@@ -1,8 +1,5 @@
 #import "UserAgent.h"
-#import "AppDelegate.h"
 #import <Cordova/CDVPluginResult.h>
-#import <Cordova/CDVViewController.h>
-#import <Cordova/CDVUserAgentUtil.h>
 
 @implementation UserAgent
 
@@ -18,20 +15,9 @@
 
 - (void)set: (CDVInvokedUrlCommand*)command
 {
-    NSString* newUserAgent = [command argumentAtIndex:0];
-    NSDictionary *dictionary = [[NSDictionary alloc] initWithObjectsAndKeys:newUserAgent, @"UserAgent", nil];
-    [[NSUserDefaults standardUserDefaults] registerDefaults:dictionary];
+    id newUserAgent = [command argumentAtIndex:0];
+    self.webView.customUserAgent = newUserAgent;
     
-    // [CDVUserAgentUtil acquireLock:^(NSInteger lockToken) {
-    //     NSString* newUserAgent = [command argumentAtIndex:0];
-    //     self.webView.customUserAgent = newUserAgent;
-    //     [CDVUserAgentUtil setUserAgent:newUserAgent lockToken:lockToken];
-    //     [CDVUserAgentUtil releaseLock:&lockToken];
-        
-    //     NSString* callbackId = command.callbackId;
-    //     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:newUserAgent];
-    //     [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
-    // }];
     NSString* callbackId = command.callbackId;
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:newUserAgent];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
